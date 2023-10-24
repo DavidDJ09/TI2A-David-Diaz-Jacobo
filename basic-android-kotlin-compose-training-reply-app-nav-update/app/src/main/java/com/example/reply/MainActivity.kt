534a53babf81f048f7a1e018dd0504945df25e05
David09 <s20120178@alumnos.itsur.edu.mx>
2023-10-23 21:40:58 -0600

    Entrega completa de Practica: Compila una app con un diseño adaptable

--- basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
+++ basic-android-kotlin-compose-training-reply-app-nav-update/app/src/main/java/com/example/reply/MainActivity.kt
@@ -13,6 +13,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// ENTREGA COMPLETA, PRACTICA TERMINADA
 package com.example.reply
 
 import android.os.Bundle
